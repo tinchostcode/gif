614191754cceb32c3da242920e76097da6fff230
me.tinchost <me.tinchost@gmail.com>
2024-02-16 08:10:48 -0300

    gifapp

---
+++ src/app/gifs/resultados/resultados.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resultados',
+  templateUrl: './resultados.component.html',
+
+})
+export class ResultadosComponent {
+
+}
